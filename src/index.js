493ce334a45c40276461dc8611547c1b16e658e6
Anthony Mendoza <anthony.mendoza310@gmail.com>
2020-09-29 15:41:11 -0700

    wrote skeleton of index.html and a pseudocode outline in index.js

---
+++ src/index.js
@@ -0,0 +1,50 @@
+/*
+Making a to-do list
+Parts:
+    TopNav
+        Expand All Tasks (Minimizes Groups)
+    Project List
+        Project Card
+            Completion Checkbox
+            Name
+            Priority
+            Due Date
+            Expand for more info
+                Description
+                Notes
+                Task Card
+                    Name
+                    Priority
+                    Due Date
+                    Expand for more info
+                        Description
+                        Notes
+                    Delete Btn (With Confirmation)
+            Delete Btn (With Confirmation)
+    Form Tab
+        Form Tab Has Two Tabs
+            Project Tab
+                Name
+                Project Type
+                    Dailies
+                    Weekly
+                    Long Term
+                Due Date
+                    Greyed Out If Daily
+                    Pick A Day of the Week if Weekly (Sunday By Default)
+                Priority
+                    Greyed Out If Daily or Weekly
+                Description
+                Notes
+            New Task Tab
+                Name
+                Due Date
+                Priority
+                Description
+                What Project They Belong To
+                Notes
+Interface?
+    Main section that contains the main list or form to add items to the list depending on what tab is currently selected
+    Nav at the bottom of the screen to jump between tabs
+    Nav at the top of the page that offers extra controls, like expanding all tasks and hiding/minimizing groupings
+*/
