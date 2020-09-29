b67e2b1b62101a39dc48f8cfffbbaf42c84eabb3
Anthony Mendoza <anthony.mendoza310@gmail.com>
2020-09-29 15:52:32 -0700

    made job.js to make task and project objects; wrote pseudocode outline

---
+++ job.js
@@ -0,0 +1,12 @@
+/*
+Write a fn to create a job
+input:
+    name, duedate, priority
+output:
+    job object
+write factory to accept name, dueDate, priority
+should also have optional values for description and notes
+write mixins to turn jobs into tasks and projects
+    task mixin will introduce project property (what project does this task belong to?)
+    project mixin will introduce project type property (daily, weekly, long term)
+*/
